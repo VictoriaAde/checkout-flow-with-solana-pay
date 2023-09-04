a078d0f743e2e52cf798a89dd6b4dac023f7ac71
Victoria <adedayovicky123@gmail.com>
2023-09-04 15:30:39 +0100

    add usdcAddress

--- src/lib/addresses.ts
+++ src/lib/addresses.ts
@@ -1,6 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
 
-// Your shop wallet address
-export const shopAddress = new PublicKey(
-  "6VnGoykg7u9MaeYpauvf9aeUJPoQVYkPUyn493FRQdz"
+// this is your shop wallet address
+export const shopAddress = new PublicKey("...");
+// this is the same for everyone!
+export const usdcAddress = new PublicKey(
+  "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"
 );
